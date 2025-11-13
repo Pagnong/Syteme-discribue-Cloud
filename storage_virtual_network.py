c44728c06d0c309b2e020066311cc2c0604492f3
Pagnong <117512366+Pagnong@users.noreply.github.com>
2025-11-13 06:09:31 +0100

    Refactor StorageVirtualNetwork for event management

--- storage_virtual_network.py
+++ storage_virtual_network.py
@@ -1,95 +1,60 @@
-from typing import Dict, Optional, Tuple
 import hashlib
 import time
-from storage_virtual_node import StorageVirtualNode, FileTransfer, TransferStatus
 from collections import defaultdict
+from typing import Dict
+from storage_virtual_node import StorageVirtualNode
+from core.transfer_manager import TransferManager
+from core.events import EventManager
 
 class StorageVirtualNetwork:
     def __init__(self):
         self.nodes: Dict[str, StorageVirtualNode] = {}
-        self.transfer_operations: Dict[str, Dict[str, FileTransfer]] = defaultdict(dict)
-        
+        self.transfer_operations = defaultdict(dict)
+        self.events = EventManager()
+        self.transfers = TransferManager(self)
+
+    # ----------------------
+    # Node & Network Control
+    # ----------------------
     def add_node(self, node: StorageVirtualNode):
-        """Add a node to the network"""
         self.nodes[node.node_id] = node
-        
+        self._emit("node_added", {"node_id": node.node_id})
+
     def connect_nodes(self, node1_id: str, node2_id: str, bandwidth: int):
-        """Connect two nodes with specified bandwidth"""
         if node1_id in self.nodes and node2_id in self.nodes:
             self.nodes[node1_id].add_connection(node2_id, bandwidth)
             self.nodes[node2_id].add_connection(node1_id, bandwidth)
+            self._emit("nodes_connected", {
+                "node1": node1_id,
+                "node2": node2_id,
+                "bandwidth": bandwidth
+            })
             return True
         return False
-    
-    def initiate_file_transfer(
-        self,
-        source_node_id: str,
-        target_node_id: str,
-        file_name: str,
-        file_size: int
-    ) -> Optional[FileTransfer]:
-        """Initiate a file transfer between nodes"""
-        if source_node_id not in self.nodes or target_node_id not in self.nodes:
-            return None
-            
-        # Generate unique file ID
-        file_id = hashlib.md5(f"{file_name}-{time.time()}".encode()).hexdigest()
-        
-        # Request storage on target node
-        target_node = self.nodes[target_node_id]
-        transfer = target_node.initiate_file_transfer(file_id, file_name, file_size, source_node_id)
-        
-        if transfer:
-            self.transfer_operations[source_node_id][file_id] = transfer
-            return transfer
-        return None
-    
-    def process_file_transfer(
-        self,
-        source_node_id: str,
-        target_node_id: str,
-        file_id: str,
-        chunks_per_step: int = 3
-    ) -> Tuple[int, bool]:
-        """Process a file transfer in chunks"""
-        if (
-            source_node_id not in self.nodes or 
-            target_node_id not in self.nodes or
-            file_id not in self.transfer_operations[source_node_id]):
-            return (0, False)
-            
-        source_node = self.nodes[source_node_id]
-        target_node = self.nodes[target_node_id]
-        transfer = self.transfer_operations[source_node_id][file_id]
-        
-         # Parallel chunk processing
-        chunks_transferred = target_node.process_chunks_parallel(
-            file_id=file_id,
-            source_node=source_node_id,
-            max_workers=chunks_per_step
-        )
-        
-        # Check if transfer is complete
-        if transfer.status == TransferStatus.COMPLETED:
-            del self.transfer_operations[source_node_id][file_id]
-            return (chunks_transferred, True)
-            
-        return (chunks_transferred, False)
-    
-    def get_network_stats(self) -> Dict[str, float]:
-        """Get overall network statistics"""
+
+    # ----------------------
+    # Helper / internal
+    # ----------------------
+    def _generate_file_id(self, file_name: str) -> str:
+        return hashlib.md5(f"{file_name}-{time.time()}".encode()).hexdigest()
+
+    def _emit(self, event_name: str, data: dict):
+        self.events.emit(event_name, data)
+
+    # ----------------------
+    # Stats
+    # ----------------------
+    def get_network_stats(self):
         total_bandwidth = sum(n.bandwidth for n in self.nodes.values())
         used_bandwidth = sum(n.network_utilization for n in self.nodes.values())
         total_storage = sum(n.total_storage for n in self.nodes.values())
         used_storage = sum(n.used_storage for n in self.nodes.values())
-        
+
         return {
             "total_nodes": len(self.nodes),
             "total_bandwidth_bps": total_bandwidth,
             "used_bandwidth_bps": used_bandwidth,
             "bandwidth_utilization": (used_bandwidth / total_bandwidth) * 100 if total_bandwidth else 0,
-            "total_storage_bytes": total_storage,
-            "used_storage_bytes": used_storage,
             "storage_utilization": (used_storage / total_storage) * 100 if total_storage else 0,
             "active_transfers": sum(len(t) for t in self.transfer_operations.values())
         }
